4b5132bc69b3c8534de81e80ec4fe8912fc26b9d
Rebeccacho1 <rebecca.cho@marsden.school.nz>
2021-07-01 09:48:37 +1200

    Finished Sprint 7. Starting Sprint 8 - Putting in an animated object

---
+++ sprint8/button.js
@@ -0,0 +1,84 @@
+console.log("button js called");
+
+// Contructor for the buttons
+class Buttons{
+    constructor(x,y,w,h,text,c1,c2,c3){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.text = text;
+        this.outline = c1;
+        this.fill = c2;
+        this.over = c3;
+        this.xMouse = 0;
+        this.yMouse = 0;
+        this.inBounds = false;
+        this.clicked = false;
+        canvas.addEventListener('click', this.mClick.bind(this));
+        canvas.addEventListener('mousemove', this.mMove.bind(this));
+    }
+
+// When the mouse is clicking - what will happen
+    mClick(e){
+    // if the mouse is clicking, it would change the button to be able to draw a rectangle or an ellipse
+    // and also be able to draw the shapes
+        if(this.inBounds){
+            this.clicked = true;
+            Buttons.shape_name = this.text;
+        }
+    }
+
+// When the mouse is moving around, the program would check if the mouse is inbounds or not
+    mMove(e){
+        this.xMouse = e.offsetX;
+        this.yMouse = e.offsetY;
+        this.inBounds = this.inBoundsCheck(this.xMouse, this.yMouse, this.x, this.y, this.w, this.h);
+    }
+
+// the function for the background rectangle - inbounds: like what will happen if it's in bounds and when it's not
+    inBoundsCheck(xM, yM, x, y, w, h){
+        if(xM > x && xM < x+w && yM > y && yM < y+h){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+// update function
+    update(){
+        this.draw();
+    }
+
+// the draw function
+    draw(){
+        // tells the program what to make the rectangle look like. The design/looks of the rectangle and ellipse
+        ctx.strokeStyle = this.outline;
+        ctx.fillStyle = this.fill;
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.w, this.h);
+
+        // only allows to draw a rectangle from inbounds
+        if(this.inBounds){
+            ctx.lineWidth = 1;
+            ctx.fillStyle = this.over;
+            ctx.fill();
+            ctx.fillStyle = this.fill;
+        }
+        else
+
+        ctx.fill();
+        ctx.stroke();
+
+        // the text styles and format
+        ctx.fillStyle = this.outline;
+        var myFont = "15px 'Trebuchet MS', 'Verdana', sans-serif";
+        ctx.textBaseline = 'middle';
+        ctx.textAlign = 'center';
+        ctx.font = myFont;
+        ctx.fillText(this.text, this.x + this.w/2, this.y + this.h/2);
+    }
+}
+Buttons.shape_name = ""
---
+++ sprint8/buttonshape.js
@@ -0,0 +1,67 @@
+console.log("buttonshape js");
+
+// A new class creating the circle buttons
+class circleButton{
+    // CircleButton: x, y, r, text, c1(rgb string), c2(rgb string)
+    constructor(x, y, r, text, c1, c2){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.text = text;
+        this.outline = c1;
+        this.line = c2;
+
+        this.element = canvas;
+        this.element.addEventListener('click', this.mClick.bind(this));
+        this.element.addEventListener('mousemove', this.mMove.bind(this));
+
+        this.xMouse = 0;
+        this.yMouse = 0;
+        this.inBounds = false;
+        this.outline_stroke = 2;
+    }
+// When the mouse clicked down
+    mClick(e){
+        //if the mouse is being clicked down within the boundary - it would allow the button to be clicked
+        if(this.inBounds){
+            circleButton.selected = this.text;
+        }
+    }
+// when the mouse is moving on the mousepad
+    mMove(e){
+        this.xMouse = e.offsetX;
+        this.yMouse = e.offsetY;
+        // it checked wether the mouse is in bounds or not
+        this.inBounds = this.inBoundsCheck(this.xMouse, this.yMouse, this.x, this.y, this.r);
+        // if the mouse is hovering something that is in bounds - the outline stroke width would change size
+        if(this.inBounds == true){
+            this.outline_stroke = 4;
+        }
+        else{
+            this.outline_stroke = 2;
+        }
+    }
+// checks if your mouse is within bounds of the circle or not. 
+    inBoundsCheck(xM,yM,x,y,r){
+        var d = Math.sqrt(Math.pow(xM-x, 2) + Math.pow(yM-y, 2));
+        // if d is less than r, the mouse is in between the bounds
+        if(d<r){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+// updates what's in the class
+    update(){
+        ctx.beginPath();
+        ctx.lineWidth = this.outline_stroke
+        ctx.ellipse(this.x, this.y, Math.abs(this.r), Math.abs(this.r), 0, 0, 2*Math.PI);
+        ctx.fillStyle = this.fill;
+        ctx.strokeStyle = this.outline;
+        ctx.stroke();
+        ctx.fill();
+    }
+}
+circleButton.selected = "";
